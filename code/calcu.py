ed2697d213dfd9f08a23c3b826aaeae3ba6dad50
camilosanchezrodriguez60-commits <camilosanchezrodriguez60@gmail.com>
2026-09-17 20:27:03 -0500

    cree una calculadora

---
+++ code/calcu.py
@@ -0,0 +1,19 @@
+def suma(valor1, valor2):
+return valor1 + valor2
+
+def resta():
+pass
+
+def multi():
+pass
+resultado_suma = (2, 5)
+
+print(f"resultado suma:{resultado_suma}")
+
+resultado_suma = suma(2, 5)
+resultado_resta = resta(2, 5)
+resultado_multi =  multi(2, 5)
+
+print(f"resultado suma:{resultado_suma}")
+print(f"resultado resta:{resultado_resta}")
+print(f"resultado multiplicacion:{resultado_multi}")
